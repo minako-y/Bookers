476ef112e0f4a6109026905104b0f8fca20b3497
minako-y <m.tp2uk@gmail.com>
2021-06-07 09:00:14 +0000

    [Finish]投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,11 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -13,12 +15,15 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
   def destroy
   end
-  
-  
-  
-  
-  
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
